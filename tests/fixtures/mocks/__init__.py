b677f51c13f906d7bc947c9a963e926dbb092594
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-12-09 02:00:21 -0300

    feat: created fake course factory

--- tests/fixtures/mocks/__init__.py
+++ tests/fixtures/mocks/__init__.py
@@ -1,4 +1,5 @@
+from .make_fake_course import make_fake_course
 from .make_fake_enrollment import make_fake_enrollment
 from .make_fake_user import make_fake_user
 
-__all__ = ["make_fake_user", "make_fake_enrollment"]
+__all__ = ["make_fake_user", "make_fake_enrollment", "make_fake_course"]
---
+++ tests/fixtures/mocks/make_fake_course.py
@@ -0,0 +1,31 @@
+from faker import Faker
+from faker.providers import person
+
+from src.domain.entities import Course, CourseProps
+
+faker = Faker()
+faker.add_provider(person)
+
+
+def make_fake_course(
+    course_id: str = faker.uuid4(),
+    name: str = faker.first_name(),
+    description: str = faker.text(),
+    category: str = faker.word(),
+    price: int = faker.pyint(),
+    instructor_id: str = faker.uuid4(),
+) -> Course:
+    course_props: CourseProps = {
+        "course_id": course_id,
+        "name": name,
+        "description": description,
+        "category": category,
+        "price": price,
+        "instructor_id": instructor_id,
+    }
+
+    course = Course(course_props)
+
+    course.lessons = ["lesson_id_1", "lesson_id_2", "lesson_id_3"]
+
+    return course
